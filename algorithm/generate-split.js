7ec891976c988960c57d5210c92359070698adfb
Koloff <koloff.anton@gmail.com>
2017-04-04 15:19:55 +0300

    generateVolume ILP model added

--- algorithm/generate-split.js
+++ algorithm/generate-split.js
@@ -1,19 +1,16 @@
 let _ = require('lodash');
-let ec = require('./exercises/exercises-collection');
 let lpSolver = require('javascript-lp-solver');
-let Model = lpSolver.Model;
-
+let ec = require('./exercises/exercises-collection');
+let mc = require('./muscles/muscles-collection');
 let getSplitType = require('./get-split-type');
 
-let mc = require('./muscles/muscles-collection');
 
-
-function generateSplit() {
-
-  // mock userData
-  let userData = {
+function generateSplit(userParameters, userPreferredMuscles) {
+  // mock user data
+  userParameters = {
     hasParameters: false,
 
+    gender: 'male',
     measuringUnit: 'metric',
     weight: null,
     height: null,
@@ -23,122 +20,141 @@ function generateSplit() {
       tuesday: false,
       wednesday: false,
       thursday: false,
-      friday: false,
+      friday: true,
       saturday: false,
       sunday: false
     }
   };
 
-
-  // mock preferred muscles
-  let preferredMuscles = [
-    mc.keys.shoulders.posteriorHead
+  userPreferredMuscles = [
+    mc.keys.shoulders.lateralHead,
+    mc.keys.chest.clavicularHead,
+    mc.keys.chest.sternalHead,
+    mc.keys.legs.calves,
+    mc.keys.legs.quadriceps
   ];
 
-  let maxPreferredMuscles = [
+  let splitType = getSplitType(userParameters);
+  console.log(splitType);
+
+
+  let preferredMuscles = [
     mc.keys.shoulders.lateralHead,
     mc.keys.chest.clavicularHead,
     mc.keys.chest.sternalHead,
   ];
-
-  let minPreferredMuscles = [
-    mc.keys.triceps.longHead
+  let trainedMuscles = [
+    mc.keys.shoulders.posteriorHead,
+    mc.keys.shoulders.lateralHead,
+    mc.keys.chest.clavicularHead,
+    mc.keys.chest.sternalHead,
   ];
+  let sets = 16;
+  let minVolumeMultiplier = 0.33;
+  let maxVolumeMultiplier = 2;
+  let minIsolationSetsCount = 1;
+  let maxExerciseSetsCount = 5;
 
-  let muscles = _.clone(mc.muscles);
-  let exercises = _.clone(ec.exercises);
+  let workout = generateVolume({
+    trainedMuscles,
+    preferredMuscles,
+    sets,
+    minVolumeMultiplier,
+    maxVolumeMultiplier,
+    minIsolationSetsCount,
+    maxExerciseSetsCount
+  });
+
+  console.log(workout);
+}
 
 
-  // Model ILPP constraints
+// TODO (optional): add max sets for each exercise
+// TODO (optional): add constraint for minimum compound lifts; add type parameter to exercise collection
+/**
+ * Get parameters object and returns workout array in the form: [{exerciseName: setsCount}]
+ * @param parameters: <Array>trainedMuscles, <Array>preferredMuscles, <Int>sets, <Float>minVolumeMultiplier, <Float>maxVolumeMultiplier, <Int>minIsolationSetsCount, <Int>maxExerciseSetsCount
+ */
+function generateVolume(parameters) {
+  let trainedMuscles = parameters.trainedMuscles;
+  let preferredMuscles = parameters.preferredMuscles;
+  let sets = parameters.sets;
+  let minVolumeMultiplier = parameters.minVolumeMultiplier;
+  let maxVolumeMultiplier = parameters.maxVolumeMultiplier;
+  let minIsolationSetsCount = parameters.minIsolationSetsCount;
+  let maxExerciseSetsCount = parameters.maxExerciseSetsCount;
+
+  let muscles = mc.muscles;
+  let exercises = ec.exercises;
+
+
+  // ILPP model constraints
   let constraints = {};
+  // ILPP model variables
+  let variables = {};
 
+  constraints.sets = {
+    max: sets
+  };
 
   let isolationExercisesForPreferredMuscles = [];
 
   muscles.forEach((muscle) => {
     constraints[muscle.key] = {};
-    constraints[muscle.key].max = (muscle.mrv * 100) / 2; // todo function
+    // do not overtrain muscles
+    constraints[muscle.key].max = (muscle.mrv * 100 /*in percentage*/) * maxVolumeMultiplier;
 
-    if (preferredMuscles.indexOf(muscle.key) >= 0) {
-      constraints[muscle.key].min = (muscle.mrv * 100) / 3;
+    // if muscle must be trained add MIN volume constraint
+    if (trainedMuscles.indexOf(muscle.key) >= 0) {
+      constraints[muscle.key].min = (muscle.mrv * 100 /*in percentage*/) * minVolumeMultiplier;
     }
-    if (maxPreferredMuscles.indexOf(muscle.key) >= 0) {
+    // if muscles is preferred, get its isolation exercise
+    if (preferredMuscles.indexOf(muscle.key) >= 0) {
       isolationExercisesForPreferredMuscles.push(getIsolationExerciseKey(muscle.key));
     }
   });
 
-  constraints.sets = {
-    max: 26  // TODO make parameter
-  };
-
 
   exercises.forEach((exercise) => {
+    // create ILPP constraints depending on the exercise
     constraints[exercise.key] = {};
-
+    // if the exercise isolates any of the maximum preferred muscles add it
     if (isolationExercisesForPreferredMuscles.indexOf(exercise.key) >= 0) {
-      constraints[exercise.key].min = 3;
+      constraints[exercise.key].min = minIsolationSetsCount;
     }
-
-    constraints[exercise.key].max = 3; // TODO make max sets for particular exercise function (can add to collection)
-  });
+    constraints[exercise.key].max = maxExerciseSetsCount;
 
 
-  // Model variables
-  let variables = {};
-
-  exercises.forEach((exercise) => {
+    // create ILPP variable for each exercise
     let exerciseVar = {};
     exerciseVar.sets = 1;
     exerciseVar[exercise.key] = 1;
 
-    let maxPreferredMusclesVolume = 0;
-    let minPreferredMusclesVolume = 0;
+    // calculate exercise volume for the muscles it trains and are chosen
+    let trainedMusclesVolume = 0;
     _.forOwn(exercise.musclesUsed, (percentage, mKey) => {
       exerciseVar[mKey] = percentage;
-      if (maxPreferredMuscles.indexOf(mKey) >= 0) {
-        maxPreferredMusclesVolume += percentage;
-      } else if (minPreferredMuscles.indexOf(mKey) >= 0) {
-        minPreferredMusclesVolume += percentage;
+      if (trainedMuscles.indexOf(mKey) >= 0) {
+        trainedMusclesVolume += percentage;
       }
     });
 
-    exerciseVar['maxPreferredMusclesVolume'] = maxPreferredMusclesVolume;
-    exerciseVar['minPreferredMusclesVolume'] = minPreferredMusclesVolume;
-
+    exerciseVar['trainedMusclesVolume'] = trainedMusclesVolume;
     variables[exercise.key] = exerciseVar;
   });
 
 
-  console.log(constraints);
-  console.log(variables);
-  console.log(isolationExercisesForPreferredMuscles);
-
-  // let result = lpSolver.MultiObjective({
-  //   optimize: {
-  //     maxPreferredMusclesVolume: 'max',
-  //     minPreferredMusclesVolume: 'min'
-  //   },
-  //   constraints: constraints,
-  //   variables: variables,
-  // });
-
-  let result = lpSolver.Solve({
-    optimize: 'maxPreferredMusclesVolume',
+  return lpSolver.Solve({
+    optimize: 'trainedMusclesVolume',
     opType: 'max',
     constraints: constraints,
     variables: variables,
   });
-
-  console.log('SOLUTION');
-  console.log('______________________________');
-
-
-  console.log(result);
 }
 
 
 function getIsolationExerciseKey(muscleKey) {
-  let currentExerciseKey;
+  let currentExerciseKey = '';
   let currentMaxPercentage = 0;
   ec.exercises.forEach((exercise) => {
     _.forOwn(exercise.musclesUsed, (percentage, mKey) => {
@@ -151,5 +167,6 @@ function getIsolationExerciseKey(muscleKey) {
 
   return currentExerciseKey;
 }
+
 
 generateSplit();
---
+++ get-user-fitness-level.js
@@ -0,0 +1,28 @@
+/**
+ * Returns number in the (0, 1) range, where 1 === biggest possible level
+ * @param userParameters gender, measuringUnit, weight, height, experience
+ */
+module.exports = function(userParameters) {
+  // start maximally and reduce
+  let level = 1;
+
+  // todo
+
+  // params = {
+  //   gender: 'male',
+  //   measuringUnit: 'metric',
+  //   weight: null,
+  //   height: null,
+  //   experience: '',
+  //   days: {
+  //     monday: false,
+  //     tuesday: false,
+  //     wednesday: false,
+  //     thursday: false,
+  //     friday: true,
+  //     saturday: false,
+  //     sunday: false
+  //   }
+  // }
+
+};
